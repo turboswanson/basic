d9b467cb0cbd751639fa8505ec0bdd5931ea87c5
Denzel Washington <turboswanson@MS-7B89-f0923ed1.localdomain>
2023-07-14 22:13:54 +0000

    added make foo to debag main.c

--- string/s21_memchr.c
+++ string/s21_memchr.c
@@ -4,12 +4,22 @@ void *s21_memchr(const void *str,int c,size_t n){
   const char *s = (const char *)str;
   int match = 0;
 
-  for(size_t i = 0; i < n;i++){
+  // for(size_t i = 0; i < n;i++){
+  //   if(*s == c){
+  //     match++;
+  //     break;
+  //   }
+  //   s++;
+  // }
+  size_t i = 0;
+
+  while(*s && i < n){
     if(*s == c){
       match++;
       break;
     }
     s++;
+    i++;
   }
 
   return match ? (void*)s:NULL;
