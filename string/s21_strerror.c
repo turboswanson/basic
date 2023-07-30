1efc5232c9fe871ff8f23b629b0cd656354b32ed
Denzel Washington <turboswanson@MS-7B89-f0923ed1.localdomain>
2023-07-30 23:32:42 +0000

    added cooment to s21_strerror

--- string/s21_strerror.c
+++ string/s21_strerror.c
@@ -2,7 +2,7 @@
 #include "s21_strerror.h"
 
 char *s21_strerror(int errnum) {
-    static char res[512] = {'\0'};
+    static char res[512] = {'\0'}; // static means res contains the value of the previous call   
     if(errnum < 0 || errnum > S21_ERRLIST_SIZE){
         fprintf(stderr,"Unknown error %d",errnum);
     }
